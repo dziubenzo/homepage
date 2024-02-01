957f5080306fe5c749c8bf7cd312465352f561af
dziubenzo <dziubenzo@gmail.com>
2024-02-01 10:47:10 +0100

    Add icons to header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import '../css/main.scss';
+
+import facebookIconSrc from '../assets/facebook.svg';
+import gitHubIconSrc from '../assets/github.svg';
+
+function loadImages() {
+  const facebookIcons = document.querySelectorAll('img[alt="Facebook Icon"]');
+  facebookIcons.forEach((icon) => {
+    icon.src = facebookIconSrc;
+  });
+  const gitHubIcons = document.querySelectorAll('img[alt="GitHub Icon"]');
+  gitHubIcons.forEach((icon) => {
+    icon.src = gitHubIconSrc;
+  });
+}
+
+loadImages();
