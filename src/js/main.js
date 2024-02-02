1ed2d91fd268aea86cf467d9750fd4671312675f
dziubenzo <dziubenzo@gmail.com>
2024-02-02 13:00:25 +0100

    Resize and crop header and footer images
    This also involves showing different images for tablets and mobile
    phones.

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,11 @@ import librarySrc from '../assets/library.png';
 import calculatorSrc from '../assets/calculator.png';
 import phoneIconSrc from '../assets/phone.svg';
 import emailIconSrc from '../assets/email.svg';
+import defaultImageFooterSrc from '../assets/image-footer.jpg';
+import smallScreenImageFooterSrc from '../assets/image-footer-tablet-mobile.jpg';
+import defaultImageHeaderSrc from '../assets/image-header.jpg';
+import tabletImageHeaderSrc from '../assets/image-header-tablet.jpg';
+import mobileImageHeaderSrc from '../assets/image-header-mobile.jpg';
 
 function loadImages() {
   const facebookIcons = document.querySelectorAll('img[alt="Facebook Icon"]');
@@ -43,6 +48,28 @@ function loadImages() {
   phoneIcon.src = phoneIconSrc;
   const emailIcon = document.querySelector('img[alt="Email Icon"]');
   emailIcon.src = emailIconSrc;
+
+  // Art direction
+  // Header
+  const imageHeaderPicture = document.querySelector('header picture');
+  const defaultImageHeader = imageHeaderPicture.querySelector('img');
+  defaultImageHeader.src = defaultImageHeaderSrc;
+  const tabletImage = imageHeaderPicture.querySelector(
+    'source[media="(min-width: 601px) and (max-width: 1000px)"]',
+  );
+  tabletImage.srcset = tabletImageHeaderSrc;
+  const mobileImage = imageHeaderPicture.querySelector(
+    'source[media="(max-width: 600px)"]',
+  );
+  mobileImage.srcset = mobileImageHeaderSrc;
+  // Footer
+  const imageFooterPicture = document.querySelector('footer picture');
+  const defaultImageFooter = imageFooterPicture.querySelector('img');
+  defaultImageFooter.src = defaultImageFooterSrc;
+  const smallScreenImage = imageFooterPicture.querySelector(
+    'source[media="(max-width: 1000px)"]',
+  );
+  smallScreenImage.srcset = smallScreenImageFooterSrc;
 }
 
 loadImages();
